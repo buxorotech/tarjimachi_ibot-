04c54be547c513fbd928ec4aadfaf69270ae7e96
buxorotech <95276000+buxorotech@users.noreply.github.com>
2021-11-30 13:05:17 +0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater,MessageHandler,CommandHandler,Filters
+from googletrans import Translator
+
+updater = Updater('2141947477:AAE0unZyWXR8SaCNingJ3xnYtuXlRLew3Lk',use_context = True )
+
+def start(updater,context):
+ updater.message.reply_text('Salom, Men Tarjimon botman!Menga tarjima qilmoqchi bolgan sozingizni jonating! ')
+ 
+def echo(updater,context):
+ usr_msg =updater.message.text
+ translator = Translator()
+ translation = translator.translate(usr_msg,dest='uz') 
+ updater.message.reply_text(translation.text)
+ 
+dp =updater.dispatcher.add_handler
+dp(CommandHandler('start',start))
+dp(MessageHandler(Filters.text,echo))
+
+updater.start_polling()
+updater.idle()
